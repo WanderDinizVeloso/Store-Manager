5b805972c12b011998cfbac381f94c04c2f1508d
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-18 11:51:22 -0300

    cria index da pasta middlewares

---
+++ middlewares/index.js
@@ -0,0 +1,9 @@
+const error = require('./error');
+const wrapper = require('./wrapper');
+const wrapperOfMiddlewares = require('./wrapperOfMiddlewares');
+
+module.exports = {
+  error,
+  wrapper,
+  wrapperOfMiddlewares,
+};
