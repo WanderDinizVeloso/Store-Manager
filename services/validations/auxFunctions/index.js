5b42aa0533ab0327cb19905e1d3bbccab6b5beb7
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-18 16:33:47 -0300

    cria index da pasta auxFunctions

---
+++ services/validations/auxFunctions/index.js
@@ -0,0 +1,19 @@
+const isCorrectSale = require('./isCorrectSale');
+const isExists = require('./isExists');
+const isFormat = require('./isFormat');
+const isFound = require('./isFound');
+const isLength = require('./isLength');
+const isNaturalNumber = require('./isNaturalNumber');
+const isNumber = require('./isNumber');
+const isStockProblem = require('./issStockProblem');
+
+module.exports = {
+  isCorrectSale,
+  isExists,
+  isFormat,
+  isFound,
+  isLength,
+  isNaturalNumber,
+  isNumber,
+  isStockProblem,
+};
