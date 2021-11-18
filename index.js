97ee0826abfee17a9e228b8388d27aa12b080047
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-18 08:57:06 -0300

    pre configura index.js do projeto

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
