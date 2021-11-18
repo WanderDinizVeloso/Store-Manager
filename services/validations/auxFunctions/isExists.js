2cbdc9e8de8b56c2135f93a0855ba06dc0682453
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-18 15:39:08 -0300

    corrige function validation isExists

--- services/validations/auxFunctions/isExists.js
+++ services/validations/auxFunctions/isExists.js
@@ -1,11 +1,11 @@
 const { StatusCodes } = require('http-status-codes');
 
-function isExist(param) {
+function isExists(param) {
   return {
     status: StatusCodes.UNPROCESSABLE_ENTITY,
     code: 'invalid_data',
-    message: `"${param}" length must be at least ${number} characters long`,
+    message: `${param} already exists`,
   };
 }
 
-module.exports = isExist;
+module.exports = isExists;
