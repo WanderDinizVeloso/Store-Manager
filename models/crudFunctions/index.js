429d478641f255822e05c8f1f91235422de37a27
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-18 13:52:57 -0300

    implementa middleware wrapper na function crudFunctions

--- models/crudFunctions/index.js
+++ models/crudFunctions/index.js
@@ -4,13 +4,15 @@ const dbCreate = require('./create');
 const dbUpdate = require('./update');
 const dbRemove = require('./remove');
 
+const { wrapper } = require('../../middlewares');
+
 function crudFunctions(collection) {
   return {
-    dbReadAll: async () => dbReadAll(collection),
-    dbReadById: async (id) => dbReadById(collection, id),
-    dbRemove: async (id) => dbRemove(collection, id),
-    dbCreate: async (doc) => dbCreate(collection, doc),
-    dbUpdate: async (doc) => dbUpdate(collection, doc),
+    dbReadAll: async () => wrapper(dbReadAll(collection)),
+    dbReadById: async (id) => wrapper(dbReadById(collection, id)),
+    dbRemove: async (id) => wrapper(dbRemove(collection, id)),
+    dbCreate: async (doc) => wrapper(dbCreate(collection, doc)),
+    dbUpdate: async (doc) => wrapper(dbUpdate(collection, doc)),
   };
 }
 
