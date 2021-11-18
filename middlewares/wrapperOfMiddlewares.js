efe45813fd7904fd927f98718d940b8159abb8e5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-18 11:50:03 -0300

    cria middleware wrapperOfMiddlewares

---
+++ middlewares/wrapperOfMiddlewares.js
@@ -0,0 +1,9 @@
+const wrapper = (handler) => async (req, res, next) => {
+  try {
+    return handler(req, res, next);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = wrapper;
