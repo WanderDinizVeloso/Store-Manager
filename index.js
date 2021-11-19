b7b0506842713572f4b61004c2174c667735e6e6
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-18 23:06:46 -0300

    corrige index e connection para correta coneccao com o banco

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 
+require('dotenv').config();
+
 const root = require('./routers/root');
 const { error } = require('./middlewares');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
 
+app.use(root);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
-app.use(root);
 
 app.use(error);
 
--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,5 @@
 const { MongoClient } = require('mongodb');
 
-require('dotenv').config();
-
 const URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/StoreManager`;
 
 const OPTIONS = {
@@ -14,11 +12,11 @@ const DB_NAME = 'StoreManager';
 let connection = null;
 
 const DBConnection = async () => {
-  const CONNECTION_ON = (await MongoClient
-    .connect(URL, OPTIONS).db(DB_NAME));
-
   if (!connection) {
-    connection = CONNECTION_ON;
+    connection = (await MongoClient.connect(
+      URL,
+      OPTIONS,
+    )).db(DB_NAME);
   }
 
   return connection;
