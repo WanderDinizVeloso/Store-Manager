3d0b2d7cedd09aa2367b9448d34859e574dc1f56
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-19 16:09:56 -0300

    cria function isValidQuantSale

---
+++ services/validations/isValidQuantSales.js
@@ -0,0 +1,15 @@
+const { isCorrectSale } = require('./auxFunctions');
+
+const isValidQuantSales = (req, _res, next) => {
+  const newSale = req.body;
+
+  const isQuantityCorrect = newSale.some(({ quantity }) =>
+    typeof quantity !== 'number'
+    || quantity <= 0);
+
+  if (isQuantityCorrect === true) return next(isCorrectSale);
+
+  next();
+};
+
+module.exports = isValidQuantSales;
