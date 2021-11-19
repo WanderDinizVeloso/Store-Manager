98755b7c84819309338bad194317ae99ef9ca174
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-19 15:19:13 -0300

    impemanta router sales no root.js

--- routers/root.js
+++ routers/root.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const products = require('./products');
+const sales = require('./sales');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/products', products);
+root.use('/sales', sales);
 
 module.exports = root;
