487fecf8207a71e692916259b9d6da84cebff912
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-11-22 17:48:04 -0300

    corrige nome da controller update do sales

--- controllers/sales/update.js
+++ controllers/sales/update.js
@@ -2,18 +2,18 @@ const { StatusCodes } = require('http-status-codes');
 
 const { update, readById } = require('../../services/sales');
 
-const updateProduct = async (req, res, _next) => {
+const updateSales = async (req, res, _next) => {
   const { id } = req.params;
   const newSale = req.body;
 
-  const product = await readById(id);
+  const sale = await readById(id);
 
   const updated = await update({
-    ...product,
+    ...sale,
     itensSold: newSale,
   });
 
   return res.status(StatusCodes.OK).json(updated);
 };
 
-module.exports = updateProduct;
+module.exports = updateSales;
